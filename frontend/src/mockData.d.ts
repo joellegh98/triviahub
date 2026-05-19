9dce73c37fc9646dea3a48b3c3d515dd5dba0471
joellegh98 <joellegharo98@gmail.com>
2026-05-19 14:58:48 +0300

    deleted mockdata

--- frontend/src/mockData.d.ts
+++
@@ -1,5 +0,0 @@
-import type { GameResult, Question, Quiz } from './types'
-
-export declare const quizzes: Quiz[]
-export declare const questions: Question[]
-export declare const gameResults: GameResult[]
